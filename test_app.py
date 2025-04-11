05da0368e50493ded95eb3066c6b37ad25b569bc
cheezzu <kkstudent404@gmail.com>
2025-04-10 21:25:32 -0700

    Add Python app with CI workflow

--- test_app.py
+++ test_app.py
@@ -1,5 +1,10 @@
 from app import add
+from app import sub
 
 def test_add():
     assert add(2,3) == 5
     assert add(4,3) == 7
+
+def test_sub():
+    assert sub(3,1) == 2
+    assert sub(5,4) == 1
